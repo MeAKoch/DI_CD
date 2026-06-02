69614d1ba410326e5cc5e77c4d12cb29d241ce78
MeAKoch <mkoch@student.tgm.ac.at>
2026-06-02 17:40:14 +0200

    helo

--- app/src/main/java/com/example/App.java
+++ app/src/main/java/com/example/App.java
@@ -7,6 +7,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello!!");
     }
 
     public int add(int a, int b) {
